0438bd11873e7c09f912107c5b470025e8deb675
Esteban-Rosas <estebanrosas94@gmail.com>
2025-05-06 23:12:08 -0500

    prueba 6

--- src/app/lugar/lugar.component.ts
+++ src/app/lugar/lugar.component.ts
@@ -13,11 +13,16 @@ export class LugarComponent {
   mostrarModalConfirmacion = false;
 
   abrirMapa() {
-    const url = 'https://maps.google.com/?q=Salón+de+eventos+y+Restaurante+EL+MIRADOR';
+    const url = 'https://maps.app.goo.gl/9qTrv2t5frB8oY238';
     window.open(url, '_blank');
   }
 
   toggleModal() {
     this.mostrarModalConfirmacion = !this.mostrarModalConfirmacion;
   }
+  abrirMapaCeremonia() {
+    const url = 'https://www.google.com/maps/dir//Cl+15+%2315,+Pasto,+Nari%C3%B1o/@1.2180142,-77.2811393,3100m/data=!3m1!1e3!4m8!4m7!1m0!1m5!1m1!1s0x8e2ed480473f8215:0xf3a48526882928d9!2m2!1d-77.2850971!2d1.2181871?entry=ttu';
+    window.open(url, '_blank');
+  }
+  
 }
